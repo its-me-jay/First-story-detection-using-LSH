9c36a1f51d26118e37fdeab1effbea9cfef531c4
its-me-jay <ee15btech11024@iith.ac.in>
2018-04-19 17:36:21 +0530

    Updated function definations

--- Shingling.py
+++ Shingling.py
@@ -1,43 +1,48 @@
 import numpy as np
+import binascii
 
-with open("stories.txt",'r') as f:
-    a=f.readlines()
-a=[x.strip() for x in a]
-
-def word_shingle(string, w):
-
+#returns the shingles
+def word_shingle(string):
+    w=3
     words = string.split()
     num_words = len(words)
 
     if w > num_words or w == 0:
         print "Number of words are less than w"
-        return
+        return [-1]
+    return [[words[i] + " " + words[i + 1] + " " + words[i + 2]] for i in range(num_words - w + 1)]
 
-    return [words[i:i + w] for i in range(num_words - w + 1)]
+#returns the 32 bit hash values of the shingles
+def word_shingle_hashes(string):
+    w=3
+    words = string.split()
+    num_words = len(words)
 
-def character_shingle(string, w):
-
-    characters = list(string)
-    num_characters = len(characters)
-
-    if w > num_characters or w == 0:
+    if w > num_words or w == 0:
         print "Number of words are less than w"
+        return [-1]
+    #return [[words[i] + " " + words[i + 1] + " " + words[i + 2]] for i in range(num_words - w + 1)]
+    return [binascii.crc32(words[i] + " " + words[i + 1] + " " + words[i + 2]) & 0xffffffff for i in range(num_words - w + 1)]
 
-    return [characters[i:i + w] for i in range(num_characters - w + 1)]
-
-shingle_set = []
-
+#Forms the shingle set with all shingles from a given set of documents. Here 'a' is an array of documents.
+#The returned shingle set is a dictionary with keys = shingles and values = ids (0 to no.of shingles)
 def formshingleSet(a):
+    shingle_set={}
     count1 = 0
+    count2=0
     for x in a:
-        shingles = word_shingle(x,3)
-        if(count1==0):
-            shingle_set = shingles
-        else:
-            shingle_set = np.append(shingle_set,shingles)
-        print "status",count1
+        output = word_shingle_hashes(x)
+        for i in output:
+          if(i!=-1):
+              try:
+                  shingle_set[i]
+              except:
+                  shingle_set[i]=count2
+                  count2+=1
+              #shingle_set.add(crc)
         count1+=1
+        print count1
+    return shingle_set
 
-formshingleSet(a)
-shingle_set = np.resize(shingle_set,(3,len(shingle_set)/3))
-print len(shingle_set)
+
+#formshingleSet(a)
